2e9476a56c6b824130082bb8e95d4c04fb65e698
Сергеев Сергей Александрович <sergeev_sa@MOSITALMED.loc>
2020-11-18 15:13:42 +0300

    уделены файлы

--- Lesson_1/clients/jivosite.py
+++
@@ -1,9 +0,0 @@
-from entities import EventCommandToSend
-
-
-class JivositeClient:
-    """Класс для работы с JivoSite"""
-
-    def send_message(self, payload: EventCommandToSend):
-        """Отпрвка сообщения"""
-        pass
--- Lesson_1/clients/ok.py
+++
@@ -1,9 +0,0 @@
-from entities import EventCommandToSend
-
-
-class OkClient:
-    """Класс для работы с Однокласниками"""
-
-    def send_message(self, payload: EventCommandToSend):
-        """Отпрвка сообщения"""
-        pass
